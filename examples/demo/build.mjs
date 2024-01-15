c4fbaad54aa24f3021569816ce949e43ec3280f7
jasmaa <jasonmaa3955@gmail.com>
2024-01-15 11:13:56 -0500

    Add .nojekyll to demo page

--- examples/demo/build.mjs
+++ examples/demo/build.mjs
@@ -6,5 +6,6 @@ const buildDir = "build";
 fs.mkdirSync(buildDir, { recursive: true });
 fs.copyFileSync("src/index.html", path.join(buildDir, "index.html"));
 fs.cpSync("src/assets", path.join(buildDir, "assets"), { recursive: true });
+fs.copyFileSync("src/.nojekyll", path.join(buildDir, ".nojekyll"));
 fs.copyFileSync("../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", path.join(buildDir, "assets/bootstrap.bundle.min.js"));
 fs.copyFileSync("../../build/rainboot.min.css", path.join(buildDir, "assets/rainboot.min.css"));
